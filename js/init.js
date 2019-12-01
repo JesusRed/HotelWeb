e6bf3bede5a0b888777f3d8a6f9b8efcff071db4
JesusRed <jesusalfredorv@ufpes.edu.co>
2019-12-01 18:37:04 -0500

    basic admin

--- js/init.js
+++ js/init.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
+    $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
+    $('#sidenav-1').sidenav({ edge: 'left' });
     $('.carousel').carousel({ duration: 100, dist: 0, shift: 50, padding: 50});
+    $('.collapsible').collapsible();
     // end of jQuery name space
 }); // end of document ready
 window.setInterval(function() { $(".carousel").carousel('next') }, 4500);
