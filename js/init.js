8f6b9576425323dd5b4bd9582e16109a991ccceb
JesusRed <jesusalfredorv@ufpes.edu.co>
2019-12-02 22:12:57 -0500

    imprimir pdf

--- js/init.js
+++ js/init.js
@@ -1,13 +1,14 @@
-$(document).ready(function(){
-    $('.datepicker').datepicker({format:'dd/mm/yyyy'});
+$(document).ready(function () {
+    $('.datepicker').datepicker({ format: 'dd/mm/yyyy' });
     $('select').formSelect();
     $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
     $('#sidenav-1').sidenav({ edge: 'left' });
-    $('.carousel').carousel({ duration: 100, dist: 0, shift: 50, padding: 50});
+    $('.carousel').carousel({ duration: 100, dist: 0, shift: 50, padding: 50 });
     $('.collapsible').collapsible();
+    
     // end of jQuery name space
 }); // end of document ready
-window.setInterval(function() { $(".carousel").carousel('next') }, 4500);
+window.setInterval(function () { $(".carousel").carousel('next') }, 4500);
 
 //{ duration: 100, dist: 0, shift: 50, padding: 50} AIzaSyAE-LlSkfSIpXI6TBVsuPPLRyvF5qA9oME
